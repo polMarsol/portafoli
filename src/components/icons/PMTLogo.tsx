353811c4938855d1c6d514214f548d696e251b46
polMarsol <polmarsoluni@gmail.com>
2026-04-25 19:24:28 +0200

    fix: use next/image for logo to respect basePath

--- src/components/icons/PMTLogo.tsx
+++ src/components/icons/PMTLogo.tsx
@@ -1,3 +1,5 @@
+import Image from "next/image";
+
 interface LogoProps {
   color?: string;
   className?: string;
@@ -5,12 +7,15 @@ interface LogoProps {
 
 export default function PMTLogo({ color: _color = "currentColor", className = "" }: LogoProps) {
   return (
-    <img
-      src="/pol-marsol-logo.svg?v=4"
+    <Image
+      src="/pol-marsol-logo.svg"
       alt="Pol Marsol logo"
+      width={40}
+      height={40}
       className={className}
       draggable={false}
       decoding="async"
+      unoptimized
     />
   );
 }
